7a02df7139c062af566ce04685de0919b206f28e
Louis Maddox <lmmx@users.noreply.github.com>
2017-07-06 15:32:02 +0100

    Create symdef.py

---
+++ symdef.py
@@ -0,0 +1,14 @@
+#spin-log⠶src⠶parse⠶symdef.py
+
+# This file provides the 'definition' to the symbol
+# syntax laid out in symsem.py and gathered into
+# a data structure in symcyl.py
+
+class symdef:
+    """..."""
+    def __init__(self):
+        self.start = 
+        self.pre = 
+        self.suff = 
+    def match(self, other):
+        # ...
