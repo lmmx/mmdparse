db9f84ed52290cbdea5f802cd20740e0b166cf6c
Louis Maddox <lmmx@users.noreply.github.com>
2017-07-06 17:24:04 +0100

    Update symdef.py

--- symdef.py
+++ symdef.py
@@ -4,11 +4,33 @@
 # syntax laid out in symsem.py and gathered into
 # a data structure in symcyl.py
 
+# N.B. 'symspec' is 'spec' as in 'specification' not
+# 'special', which here denotes four particular values
+
+specials = ['-*','x',' ','']
+
 class symdef:
-    """..."""
-    def __init__(self):
-        self.start = 
-        self.pre = 
-        self.suff = 
+    """Defined behaviour for symbol parsing
+    according to whether any are in the 'special'
+    class, or else to use standard string matching."""
+    def __init__(self, symspec):
+        self.pre = symspec.sympre
+        self.suff = symspec.symsuff
+        self.p_special = special(self.pre)
+        self.s_special = special(self.suff)
     def match(self, other):
-        # ...
+        if self.p_special is None:
+            # use self.pre
+        else:
+            # use self.p_special
+        if self.s_special is None:
+            # use self.suff
+        else:
+            # use self.s_special
+
+class special:
+    """Symbols with non-standard interpretations:
+    '-*' (asterisk: wildcard), ' ' (space: ),
+    'x' (cross: omit pattern), '' (empty: no content)."""
+    def __init__(self, symbol):
+        self.symdict = 
